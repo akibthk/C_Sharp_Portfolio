4d3c580a78d358c11dc3361a3a6a9b09e709a0e2
akibthk <55997809+akibthk@users.noreply.github.com>
2020-01-20 14:12:17 -0500

    Add files via upload

---
+++ thkhanPortfolio/Binary calculator/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            string output = "";
+            if (Convert.ToDouble(textBox1.Text) <= 0)
+            {
+                MessageBox.Show("Insert a positive number");
+            }
+            else
+            {
+            try
+            {
+                int input = Convert.ToInt32(textBox1.Text);
+                while (input != 0)
+                {
+                    int r = input % 2;
+                    output += r.ToString();
+                    input = input / 2;
+                }
+                for (int i = output.Length - 1; i >= 0; i--)
+                {
+                    label3.Text += output[i];
+                }
+            }
+            catch { }
+            if (output == "")
+            {
+                MessageBox.Show("Input error");
+            }
+        }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label3.Text = "";
+            textBox1.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ thkhanPortfolio/Truncation/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string outputFinal = "";
+            try
+            {
+                double val1 = Convert.ToDouble(textBox1.Text);
+                int precision = Convert.ToInt32(comboBox1.Text);
+                double output = 0;
+                double output2 = 0;
+                for (int i = 0; i < (Convert.ToString(val1).Length); i++)
+                {
+                    if (Convert.ToString(val1).Substring(i, 1) == ".")
+                    {
+                        try
+                        {
+                            output = Convert.ToDouble(Convert.ToString(val1).Substring(0, i));
+                            output2 = Convert.ToDouble(Convert.ToString(val1).Substring(i + 1, precision));
+                            outputFinal = Convert.ToString((output + "." + output2));
+                            label4.Text = outputFinal;
+                        }
+                        catch { }
+                    }
+                }
+            }
+            catch { }
+            if (outputFinal == "")
+            {
+                MessageBox.Show("Input error");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
